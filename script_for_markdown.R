07b9175919f913eb50195a592416d27c0d74a525
Aditya Ranganath <aranganath24@gmail.com>
2021-12-09 11:01:04 -0700

    adds wordcloud

--- script_for_markdown.R
+++ script_for_markdown.R
@@ -106,14 +106,32 @@ CancelStudentDebt_coinciding_hashtags<-student_debt_tweets %>%
 
 
 
-ggplot(CancelStudentDebt_coinciding_hashtags, aes(x=reorder(hashtags, n), y=n))+
-  geom_bar(stat="identity")+
-  coord_flip()+
-  xlab("")+
-  ylab("Number of Ocurrences with #CancelStudentDebt")+
-  ggtitle("Test")
 
 # Visualizing and Exploring Data ------------------------------------------
+
+# Using ggplot to make visualizations of twitter data: bar graph of coincident hashtags
+
+CancelStudentDebt_coinciding_hashtags<-CancelStudentDebt_coinciding_hashtags %>% 
+                                        mutate(hashtag=paste0("#", hashtags))
+
+coincident_hashtags_plot<-
+  ggplot(CancelStudentDebt_coinciding_hashtags, aes(x=reorder(hashtag, n), y=n))+
+    geom_bar(stat="identity")+
+      coord_flip()+
+      xlab("")+
+      ylab("Frequency")+
+      ggtitle("Hashtags Most Frequently Used Along With #CancelStudentDebt")+
+      labs(caption = "Data Collected from Twitter REST API via rtweet")
+
+coincident_hashtags_plot
+
+# Using rtweet's visualization functions: time series
+ts_plot(student_debt_tweets, "hours") +
+  labs(x = NULL, y = NULL,
+       title = "Frequency of tweets with a #CancelStudentDebt hashtag",
+       subtitle = paste0(format(min(student_debt_tweets$created_at), "%d %B %Y"), " to ", format(max(student_debt_tweets$created_at),"%d %B %Y")),
+       caption = "Data collected from Twitter's REST API via rtweet") +
+  theme_minimal()
 
 
 ## BLM Word Cloud 
