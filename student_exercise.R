f35a2c9c5c7bfeb53cb7765cbbc514341ad59355
Aditya Ranganath <aranganath24@gmail.com>
2021-12-09 19:59:12 -0700

    adds sample code for student exercise

--- student_exercise.R
+++ student_exercise.R
@@ -1,31 +1,7 @@
-CancelStudentDebt_coinciding_hashtags<-student_debt_tweets %>% 
-  select(hashtags) %>% 
-  unnest(hashtags) %>%
-  mutate(hashtag_cleaned=str_to_lower(hashtags)) %>% 
-  filter(hashtag_cleaned!="cancelstudentdebt") %>% 
-  select(-hashtag_cleaned) %>% 
-  count(hashtags) %>% 
-  slice_max(n, n=10)
 
-View(CancelStudentDebt_coinciding_hashtags)
-
-# Visualizing and Exploring Data ------------------------------------------
-
-# Using ggplot to make visualizations of twitter data: bar graph of coincident hashtags
-
-CancelStudentDebt_coinciding_hashtags<-CancelStudentDebt_coinciding_hashtags %>% 
-  mutate(hashtag=paste0("#", hashtags))
-
-coincident_hashtags_plot<-
-  ggplot(CancelStudentDebt_coinciding_hashtags, aes(x=reorder(hashtag, n), y=n))+
-  geom_bar(stat="identity")+
-  coord_flip()+
-  xlab("")+
-  ylab("Frequency")+
-  ggtitle("Hashtags Most Frequently Used Along With #CancelStudentDebt")+
-  labs(caption = "Data Collected from Twitter REST API via rtweet")
-
-coincident_hashtags_plot
+# Make function to take inputs of twitter hashtag and number of queries, and create a bar chart
+# of the hashtags occuring most frequently alongside the input hashtag; then, iterate this function 
+# across several hashtags and put the resulting visualizations in a list
 
 top_hashtags_plot<-function(twitterhashtag, tweetnumber){
   
@@ -43,30 +19,43 @@ hashtag_frequency_table<-
     filter(hashtag_cleaned!=str_to_lower(twitterhashtag)) %>% 
     select(-hashtag_cleaned) %>% 
     count(hashtags) %>% 
-    slice_max(n, n=10)
+    slice_max(n, n=10, with_ties = FALSE)
 
-return(hashtag_frequency_table)
+hashtag_frequency_table<-hashtag_frequency_table %>% mutate(hashtag=paste0("#", hashtags))
+
+related_hashtags_plot<-
+  ggplot(hashtag_frequency_table, aes(x=reorder(hashtag, n), y=n))+
+  geom_bar(stat="identity")+
+  coord_flip()+
+  xlab("")+
+  ylab("Frequency")+
+  ggtitle(paste0("Hashtags Most Frequently Used Along With", " ", "#", twitterhashtag))+
+  labs(caption = "Data Collected from Twitter REST API via rtweet")
+
+return(related_hashtags_plot)
+
 }
 
+# Test function
+top_hashtags_plot("SupremeCourt", 1000)
+top_hashtags_plot("ZeroCovid", 1000)
 
+# Iterate and put visualizations into list
 
+desired_hashtags<-c("ZeroCovid", "teslaq", "airbnb", "trump")
+number<-c(1000)
 
+hashtag_viz_list<-map2(.x=desired_hashtags, .y=number, .f=top_hashtags_plot)
 
+hashtag_viz_list[[2]]
+hashtag_viz_list[[4]]
+hashtag_viz_list[[1]]
+hashtag_viz_list[[3]]
 
+# Assig names
+names(hashtag_viz_list)<-desired_hashtags
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+hashtag_viz_list
 
 
 
